6c89dff7d8cd7db1104335a7f3c67f7052e74aca
Insafka16 <Robo.Crafter@yandex.ua>
2017-03-31 20:56:07 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,9 @@
+package com.company;
+
+public class Main {
+    public static void main(String[] args) {
+        SimpleGUI app = new SimpleGUI();
+        app.setTitle("GGGG");
+        app.setVisible(true);
+    }
+}
